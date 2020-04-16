7682c8caf064915516b48082eec6394e1b9e4aa0
edensMind <dje2402@rit.edu>
2020-04-15 20:16:01 -0400

    unit data in card

--- src/client/components/UnitCards.js
+++ src/client/components/UnitCards.js
@@ -38,47 +38,89 @@ function UnitCard(props) {
                 {unit.name}
             </Typography >
             <Grid container className={classes.rootGrid} spacing={5}>
-                <Grid item xs>
+                <Grid item xs={2}>
                     <div>
-                        <p>Type: {`${unit.subType} ${unit.type}`}</p>
-                        <p>Challenge Rating: {unit.challengeRating}</p>
-                        <p>Size: {unit.size}</p>
+                        Type: {`${unit.subType} ${unit.type}`}<br/>
+                        Challenge Rating: {unit.challengeRating}<br/>
+                        Size: {unit.size}
+                    </div>
+                </Grid>
+                <Grid item xs={2}>
+                    <div>
+                        Energy AC: {unit.energyArmorClass}<br/>
+                        Kinetic AC: {unit.kineticArmorClass}<br/>
+                        Fortitude: {unit.savingThrows.fortitude}<br/>
+                        Reflex: {unit.savingThrows.reflex}<br/>
+                        Will: {unit.savingThrows.will}<br/>
                     </div>
                 </Grid>
                 <Grid item xs>
                     <div>
-                        <p>Energy AC: {unit.energyArmorClass}</p>
-                        <p>Kinetic AC: {unit.kineticArmorClass}</p>
+                        {/* str: {unit.abilityScores.str}<br/>
+                        dex: {unit.abilityScores.dex}<br/>
+                        con: {unit.abilityScores.con}<br/>
+                        int: {unit.abilityScores.int}<br/>
+                        wis: {unit.abilityScores.wis}<br/>
+                        cha: {unit.abilityScores.cha} */}
+                        <table>
+                            <tr>
+                                <th>ABI</th>
+                                <th>SCR</th>
+                                <th>MOD</th>
+                            </tr>
+                            <tr>
+                                <th>STR</th>
+                                <td>{unit.abilityScores.str}</td>
+                                <td>+{getAbilityScoreModifier(unit.abilityScores.str)}</td>
+                            </tr>
+                            <tr>
+                                <th>DEX</th>
+                                <td>{unit.abilityScores.dex}</td>
+                                <td>+{getAbilityScoreModifier(unit.abilityScores.dex)}</td>
+                            </tr>
+                            <tr>
+                                <th>CON</th>
+                                <td>{unit.abilityScores.con}</td>
+                                <td>+{getAbilityScoreModifier(unit.abilityScores.con)}</td>
+                            </tr>
+                            <tr>
+                                <th>INT</th>
+                                <td>{unit.abilityScores.int}</td>
+                                <td>+{getAbilityScoreModifier(unit.abilityScores.int)}</td>
+                            </tr>
+                            <tr>
+                                <th>WIS</th>
+                                <td>{unit.abilityScores.wis}</td>
+                                <td>+{getAbilityScoreModifier(unit.abilityScores.wis)}</td>
+                            </tr>
+                            <tr>
+                                <th>CHA</th>
+                                <td>{unit.abilityScores.cha}</td>
+                                <td>+{getAbilityScoreModifier(unit.abilityScores.cha)}</td>
+                            </tr>
+                        </table>
                     </div>
                 </Grid>
-                <Grid item xs>
+                <Grid item xs={3}>
                     <div>
-                    str: {unit.abilityScores.str}<br/>
-                    dex: {unit.abilityScores.dex}<br/>
-                    con: {unit.abilityScores.con}<br/>
-                    int: {unit.abilityScores.int}<br/>
-                    wis: {unit.abilityScores.wis}<br/>
-                    cha: {unit.abilityScores.cha}
+                        {unit.weapons.map((weapon) => (
+                        <div>
+                            {weapon.name}<br/>
+                            <span>{weapon.damage} {weapon.type} damage</span><br/>
+                            <span>Bonus: +{weapon.attackBonus} Range: {weapon.range} Critical: {weapon.critical}x</span>
+                        </div>
+                        ))}
                     </div>
                 </Grid>
-                <Grid item xs>
+                <Grid item xs={3}>
                     <div>
-                        <p>Energy AC: {unit.energyArmorClass}</p>
-                        <p>Kinetic AC: {unit.kineticArmorClass}</p>
-                    </div>
-                </Grid>
-                <Grid item xs>
-                    <div>
-                        <p>Type: {`${unit.subType} ${unit.type}`}</p>
-                        <p>Challenge Rating: {unit.challengeRating}</p>
-                        <p>Size: {unit.size}</p>
-                    </div>
-                </Grid>
-                <Grid item xs>
-                    <div>
-                        <p>Type: {`${unit.subType} ${unit.type}`}</p>
-                        <p>Challenge Rating: {unit.challengeRating}</p>
-                        <p>Size: {unit.size}</p>
+                        Spell Uses {unit.spellUses}
+                        {unit.spells.map((spell) => (
+                        <div>
+                            <span>{spell.name} lvl {spell.level}</span><br/>
+                            <span>{spell.description}</span>
+                        </div>
+                        ))}
                     </div>
                 </Grid>
 
@@ -87,6 +129,11 @@ function UnitCard(props) {
         </CardContent>
     </Card>
     );
+}
+
+const getAbilityScoreModifier = (score) => {
+    var mod = Math.floor(( score - 10 ) / 2 )
+    return mod;
 }
 
 // Class component with update / fetch functions
@@ -99,9 +146,20 @@ export default class UnitCards extends React.Component {
         super(props);
 
         this.state = { 
+            hasError: false,
             encounterId: props.encounterId,
             units: [] 
         };
+    }
+
+    static getDerivedStateFromError(error) {
+        // Update state so the next render will show the fallback UI.
+        return { hasError: true };
+      }
+
+    componentDidCatch(error, errorInfo) {
+        // You can also log the error to an error reporting service
+        logErrorToMyService(error, errorInfo);
     }
 
     fetchEncounter() {
@@ -114,12 +172,12 @@ export default class UnitCards extends React.Component {
                 this.setState({units: units, encounterId: thisComp.props.encounterId});
             }
             else if(res.status == 204){
-                console.log("getAllEncounters 204", res.status);
-            }
-            else {
-                console.log("getAllEncounters OTHER_STATUS", res.status);
+                console.log("getEncounter 204", res.status);
             }
         })
+        .catch(error => {
+            console.log("!getEncounter: ", error);
+        });
     }
 
     componentDidUpdate(prevProps) {
@@ -129,12 +187,17 @@ export default class UnitCards extends React.Component {
     }
 
     render() {
-        return (
-            <>
-                {this.state.units.map((unit) => (
-                    <UnitCard unit={unit} key={unit._id}/>
-                ))}
-            </>
-        );
+        if (this.state.hasError) {
+            <div>Error Rendering Cards</div>
+        }
+        else {
+            return (
+                <>
+                    {this.state.units.map((unit) => (
+                        <UnitCard unit={unit} key={unit._id}/>
+                    ))}
+                </>
+            );
+        }
     }
 }
